1ab36e6154ec4c7fb218cdbafd76a31a310d8983
nurdinaffandidev <nurdinaffandidev@gmail.com>
2025-08-05 09:25:55 +0800

    feat[service]: update methods for question related logics

--- src/main/java/com/nurdinaffandidev/quiz_service/service/QuizService.java
+++ src/main/java/com/nurdinaffandidev/quiz_service/service/QuizService.java
@@ -1,7 +1,7 @@
 package com.nurdinaffandidev.quiz_service.service;
 
 import com.nurdinaffandidev.quiz_service.exception.QuizNotFoundException;
-import com.nurdinaffandidev.quiz_service.model.Question;
+import com.nurdinaffandidev.quiz_service.feign.QuizInterface;
 import com.nurdinaffandidev.quiz_service.model.QuestionWrapper;
 import com.nurdinaffandidev.quiz_service.model.Quiz;
 import com.nurdinaffandidev.quiz_service.model.QuizResponse;
@@ -9,28 +9,24 @@ import com.nurdinaffandidev.quiz_service.repository.QuizRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 @Service
 public class QuizService {
     private final QuizRepository quizRepository;
+    private final QuizInterface quizInterface;
 
     @Autowired
-    public QuizService(QuizRepository quizRepository) {
+    public QuizService(QuizRepository quizRepository, QuizInterface quizInterface) {
         this.quizRepository = quizRepository;
+        this.quizInterface = quizInterface;
     }
 
     public Quiz createQuiz(String category, Integer numQuestions, String title) {
-        // connect to question-service to retrieve questions
-        // - via RestTemplate
-        // - through Feign Client, Service Discovery, Eureka Server
-
-//        List<Question> questions = questionService.generateQuestions(category, numQuestions);
+        List<Integer> questionsIds = quizInterface.generateQuestionsIds(category, numQuestions).getBody();
         Quiz quiz = new Quiz();
         quiz.setTitle(title);
-//        quiz.setQuestions(questions);
+        quiz.setQuestionIds(questionsIds);
         quizRepository.save(quiz);
         return quiz;
     }
@@ -42,49 +38,11 @@ public class QuizService {
     public List<QuestionWrapper> getQuizQuestions(Integer id) {
         Quiz quiz = quizRepository.findById(id)
                 .orElseThrow(() -> new QuizNotFoundException("Quiz with id= " + id + " not found."));
-
-        List<QuestionWrapper> cleanedQuestions = new ArrayList<>();
-        quiz.getQuestions().forEach( question -> {
-            QuestionWrapper cleanedQn = new QuestionWrapper(
-                    question.getId(),
-                    question.getOption1(),
-                    question.getOption2(),
-                    question.getOption3(),
-                    question.getOption4(),
-                    question.getQuestionTitle()
-                );
-            cleanedQuestions.add(cleanedQn);
-        });
-        return cleanedQuestions;
+        return quizInterface.retrieveWrapperQuestions(quiz.getQuestionIds()).getBody();
     }
 
-    public Integer calculateResult(Integer quizId, List<QuizResponse> responses) {
-
-        // connect to question-service to retrieve questions
-        // - via RestTemplate
-        // - through Service Discovery
-
-        int correctAnswer = 0;
-
-        Quiz quiz = quizRepository.findById(quizId)
-                .orElseThrow(() -> new QuizNotFoundException("Quiz with id= " + quizId + " not found."));
-
-        List<Question> quizQuestions = quiz.getQuestions();
-        System.out.println("quizQuestions= ");
-        quizQuestions.forEach(System.out::println);
-
-        for(QuizResponse response : responses) {
-            System.out.println("response= " + response);
-            Optional<Question> questionToCheck = quizQuestions.stream()
-                    .filter(qn -> qn.getId() == response.getQuestionId())
-                    .findFirst();
-            if(questionToCheck.isPresent()) {
-                if (response.getResponse().equals(questionToCheck.get().getCorrectAnswer())) {
-                    correctAnswer++;
-                }
-            }
-        }
-        return correctAnswer;
+    public Integer calculateResult(List<QuizResponse> responses) {
+        return quizInterface.getScore(responses).getBody();
     }
 
 }
